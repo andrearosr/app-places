87237fb0613785ba2767bc781b37212d42c63ff2
Andrea Rosales <andrearosr@gmail.com>
2022-03-21 19:40:47 -0500

    Initialize database

--- App.js
+++ App.js
@@ -6,6 +6,15 @@ import store from './store'
 // navigation
 import MainNavigator from './navigation'
 
+import { init } from './db';
+
+init()
+  .then(() => { console.log('Database initialized') })
+  .catch((err) => {
+    console.log('Database failed to connect')
+    console.log(err.message)
+  })
+
 export default function App() {
   return (<Provider store={store}><MainNavigator /></Provider>);
 }
